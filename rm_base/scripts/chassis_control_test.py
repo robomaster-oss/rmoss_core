ff31a26b6c61c331e2a1640044e5fbc45494882f
zhenpeng ge <zhenpeng.ge@qq.com>
2020-10-12 16:58:48 +0800

    update rm_base

--- rm_base/scripts/chassis_control_test.py
+++ rm_base/scripts/chassis_control_test.py
@@ -131,7 +131,7 @@ def main():
     rclpy.init()
 
     node = rclpy.create_node('teleop_twist_keyboard')
-    pub = node.create_publisher(geometry_msgs.msg.Twist, 'cmd_vel', 10)
+    pub = node.create_publisher(geometry_msgs.msg.Twist, '/chassis_control', 10)
 
     speed = 0.5
     turn = 1.0

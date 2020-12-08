52038afb8f625d4973a5026d9eb6ec9b766cf8ed
zhenpeng ge <zhenpeng.ge@qq.com>
2020-12-08 12:59:18 +0800

    DEBUG_TOOL is deprecated

--- rm_common/include/rm_common/debug.hpp
+++ rm_common/include/rm_common/debug.hpp
@@ -14,7 +14,7 @@
 #include <opencv2/opencv.hpp>
 
 #define DEBUG_TOOL(text)            \
-    std::cout<<"DEBUG_TOOL() is deprecated,please use RM_DEBUG()"<<std::endl;\
+    std::cout<<"DEBUG_TOOL() is deprecated,please use RM_DEBUG()."<<std::endl;\
     if (rm_common::isDebug()) \
         text
 
